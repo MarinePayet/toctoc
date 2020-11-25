45dc1a8b2d7dc9b2e8812ba39c60fba0c34dc7c1
Chiara de Montlaur <chiarademontlaur@gmail.com>
2020-11-25 16:10:03 +0100

    seed done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  CATEGORIES = ["SOS", "Tips", "FYI","Lost & Founds","Issues"]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,3 @@
-require 'faker'
 require 'open-uri'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
@@ -9,10 +8,19 @@ require 'open-uri'
 #   Character.create(name: 'Luke', movie: movies.first)
 puts "destroying event"
 Event.destroy_all
-puts "destroying user"
-User.destroy_all
+puts "destroying messages"
+Message.destroy_all
+puts "destroying participants"
+Participant.destroy_all
 puts "destroying inbox"
 Inbox.destroy_all
+puts "destroying services"
+Service.destroy_all
+puts "destroying posts"
+Post.destroy_all
+puts "destroying user"
+User.destroy_all
+
 
 puts "start user"
 
@@ -26,7 +34,7 @@ users = []
     password: Faker::Code.npi,
     stairs: Faker::Number.between(from: 1, to: 10),
     birthday: Faker::Date.birthday,
-    catch_phrase: Faker::Quotes::Shakespeare.romeo_and_juliet_quote,
+    catch_phrase: Faker::TvShows::HowIMetYourMother.catch_phrase,
     hobbies: [User::HOBBIES.sample],
     job: Faker::Job.title,
     phone_number: Faker::PhoneNumber
@@ -38,21 +46,79 @@ puts "user finish"
 
 puts "start events"
 10.times do
+  start_date = DateTime.parse(Faker::Time.backward(days: 7, format: :short))
+  end_date = (start_date + rand(1..5).days).beginning_of_day
   @event_new = Event.create!(
-    starting_date: Faker::Time.backward(days: 7, format: :short),
-    ending_date: Faker::Time.forward(days: 5, format: :short),
+    starting_at: start_date,
+    ending_at: end_date + rand(0..23).hours + rand(0..59).minutes,
     location: [Event::LOCATIONS.sample],
     title: [Event::TITLES.sample],
     description: Faker::Movie.quote,
     user: users.sample
     )
 end
-
 puts "end events"
 
 puts "start inboxes"
-​10.times do
-  @inbox_new = Inbox.create!(
-    topic: Faker::ChuckNorris.fact
+
+inboxes = []
+
+
+inbox_new = Inbox.create!(
+  topic: Faker::ChuckNorris.fact
+)
+
+inboxes << inbox_new
+
+puts "end inboxes"
+
+puts "start participants"
+
+users.first(5).each do |user|
+  participant_new = Participant.create!(
+  user: user,
+  inbox: inboxes.sample
+  )
+end
+
+puts "end participants"
+
+puts "start messages"
+10.times do
+  message_new = Message.create!(
+  content: Faker::Movies::HarryPotter.quote,
+  user: users.first(5).sample,
+  inbox: inboxes.sample,
     )
 end
+
+puts "end messages"
+
+puts "start services "
+
+10.times do
+  service_new = Service.create!(
+  available: [true, false].sample,
+  name: [Service::NAMES].sample,
+  description: Faker::TvShows::Friends.quotes,
+  price: rand(0..200),
+  user: users.sample
+  )
+end
+
+puts "end services"
+
+puts "start posts"
+
+10.times do
+  post_new = Post.create!(
+  title: Faker::TvShows::HowIMetYourMother.quote,
+  category: [Post::CATEGORIES].sample,
+  content: Faker::TvShows::TheFreshPrinceOfBelAir.quote,
+  user: users.sample
+  )
+end
+
+puts "end posts"
+
+
