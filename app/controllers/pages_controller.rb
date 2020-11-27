c341de6079d206ed0b379eb6f9a947dbc4888cee
marine payet <marine.payet3@gmail.com>
2020-11-27 12:57:28 +0100

    ok past event

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,8 @@ class PagesController < ApplicationController
   def home
     @events = Event.all.order(created_at: :desc).first(3)
     @posts = Post.all.order(created_at: :desc).first(3)
+
+
   end
 
   def choice
