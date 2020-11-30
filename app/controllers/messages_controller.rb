635a0ce287d1543adbc6f698e2c32bd4eb6c6a8d
Chiara de Montlaur <chiarademontlaur@gmail.com>
2020-11-30 12:40:05 +0100

    messages and inboxes

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,9 @@
 class MessagesController < ApplicationController
 
   def index
-     @messages = policy_scope(Message).order(created_at: :desc)
+    @inbox = Inbox.find(params[:inbox_id])
+    @messages = Message.where(inbox_id: @inbox.id)
+    @messages = policy_scope(Message).order(created_at: :desc)
      # @messages = Message.all
   end
 
@@ -16,9 +18,16 @@ class MessagesController < ApplicationController
   # end
 
   def create
+    @inbox = Inbox.find(params[:inbox_id])
     @message = Message.new(message_params)
+    @message.inbox = @inbox
+    @message.user = current_user
     authorize @message
-    @message.save
+    if @message.save
+      redirect_to inbox_path(@inbox, anchor: "message-#{@message.id}")
+    else
+      render "inbox/show"
+    end
   end
 
 private
--- app/models/inbox.rb
+++ app/models/inbox.rb
@@ -2,4 +2,5 @@ class Inbox < ApplicationRecord
   has_many :messages
   has_many :participants
   has_one :user
+  attr_accessor :message
 end
