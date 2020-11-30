ba5a5ab8ec43b1a1d1ba397fe62490fdc676186f
marine payet <marine.payet3@gmail.com>
2020-11-30 12:31:39 +0100

    search ok

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -2,31 +2,34 @@ class ServicesController < ApplicationController
 
 
   def index
-    @services = Service.none
-      if params[:loan] == "true"
-        @services = @services.or(Service.where(:typology=> "Loan"))
-      end
-      if params[:service] == "true"
-        @services = @services.or(Service.where(:typology=> "Service"))
-      end
-    @services = Service.all if @services.empty?
-      if params[:available] == "true"
-        @services = @services.where(:available=> true)
-      end
-    @services = policy_scope(@services).order(created_at: :desc)
 
-
-if params[:query].present?
-      sql_query = " \
-        service.name @@ :query \
-        OR service.description @@ :query \
-        OR service.price @@ :query \
-        OR service.available @@ :query \
-      "
-      @services = Service.where(sql_query, query: "%#{params[:query]}%")
+    if params[:query].present?
+      @services = Service.search_by_name_and_description(params[:query])
+      # sql_query = " \
+      #   services.name @@ :query \
+      #   OR services.description @@ :query
+      # "
+      # @services = Service.where(sql_query, query: "%#{params[:query]}%")
     else
       @services = Service.all
     end
+
+
+    # @services = Service.none
+      if params[:loan] == "true"
+        @services = @services.where(typology: "Loan")
+        # @services = @services.or(Service.where(:typology=> "Loan"))
+      end
+      if params[:service] == "true"
+        @services = @services.where(typology: "Service")
+        # @services = @services.or(Service.where(:typology=> "Service"))
+      end
+    # @services = Service.all if @services.empty?
+      if params[:available] == "true"
+        @services = @services.where(:available=> true)
+      end
+
+    @services = policy_scope(@services).order(created_at: :desc)
 
 
   end
@@ -34,6 +37,7 @@ if params[:query].present?
   def show
     @service = Service.find(params[:id])
     authorize @service
+
   end
 end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,17 @@
 class UsersController < ApplicationController
+  def index
+    if params[:query].present?
+      @users = User.search_by_names_job_and_hobbies(params[:query])
+
+    else
+      @users = User.all
+    end
+
+  @users = policy_scope(@users).order(created_at: :desc)
+  end
+
   def show
     @user = User.find(params[:id])
   end
+
 end
