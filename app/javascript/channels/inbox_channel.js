d09c421bba3f70e635545f425ab433b9dc1d19c9
Chiara de Montlaur <chiarademontlaur@gmail.com>
2020-12-01 17:52:19 +0100

    notifications

--- app/javascript/channels/inbox_channel.js
+++ app/javascript/channels/inbox_channel.js
@@ -6,10 +6,17 @@ const initInboxCable = () => {
   if (alert) {
 
     const id = alert.dataset.userId;
+    const count = document.querySelector("#notif_count")
 
     consumer.subscriptions.create({ channel: "InboxChannel", id: id }, {
       received(data) {
-        console.log("hello", data)
+        document.querySelector('#notif_icon').classList.add("active")
+        if (count.innerText === "") {
+          count.innerText = '1'
+        } else {
+          count.innerText = parseInt(count.innerText, 10) + 1
+        }
+
         alert.querySelector('#message').innerHTML = data
       },
     });
