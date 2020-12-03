040bcb82d06f00504f51154bd4b0bdf9d89a91c6
marine payet <marine.payet3@gmail.com>
2020-12-03 15:48:58 +0100

    tri ok

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   def home
     @user = current_user
-    @events = Event.all.order(created_at: :desc).first(3)
+    @events = Event.all.order(starting_at: :asc).last(3)
     @posts = Post.all.order(created_at: :desc).first(3)
   end
 
